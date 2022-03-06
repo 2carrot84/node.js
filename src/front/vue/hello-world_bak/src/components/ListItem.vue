4012c046f73c630fb652fb9357b5eeeb48273054
[전시개발팀]이창근 <changgeun.lee@lotte.net>
2022-03-06 03:03:52 +0000

    feat: front ch5-10

---
+++ src/front/vue/hello-world_bak/src/components/ListItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <li>
+    {{ text }}
+    <button @click="itemDelete">삭제</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    index: Number,
+    text: String
+  },
+  name: "ListItem",
+  methods: {
+    itemDelete() {
+      this.$emit("delete", this.index);
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/front/vue/hello-world_bak/src/components/TodoList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div>
+      <input type="text" v-model="inputValue" />
+      <button @click="addItem">추가하기</button>
+    </div>
+    <ul>
+      <ListItem v-for="(value, index) in listArray"
+                :key="index"
+                :index="index"
+                :text="value === '' ? undefined : value"
+                @delete="catchDeleteItem"
+      ></ListItem>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ListItem from "@/components/ListItem";
+
+export default {
+  data() {
+    return {
+      inputValue: "",
+      listArray: []
+    };
+  },
+  name: "TodoList",
+  components: { ListItem },
+  methods: {
+    addItem() {
+      this.listArray.push(this.inputValue);
+      this.inputValue = "";
+    },
+    catchDeleteItem(index) {
+      this.listArray.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
